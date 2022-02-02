c37f166411a429e75a0db354379b61eabf5a6b9a
Robert Shipley <shipleymrobert@gmail.com>
2022-02-02 11:34:11 +0000

    added prettier and gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import "./App.css";
 
 const App = () => (
-    <div className="App">
-        <header className="App-header">
-            <p>Test for my portfolio page.</p>
-        </header>
-    </div>
+  <div className="App">
+    <header className="App-header">
+      <p>Test for my portfolio page.</p>
+    </header>
+  </div>
 );
 
 export default App;
